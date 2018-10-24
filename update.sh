ec3b3c8691ceffd4954887416b33f70ac8b58cab
BBK <BeanBagKing@users.noreply.github.com>
2018-10-24 19:01:54 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,59 +1,70 @@
 #!/bin/bash
 
-# Time on VM's is often off after I resume. If you don't need this, comment it out
-echo "----- UPDATING TIME -----"
+# Updated 24 Oct 2018
+# for for Kali 2018.3
+
+RED='\033[1;31m'
+GRN='\033[1;32m'
+YEL='\033[1;33m'
+BLU='\033[1;34m'
+NC='\033[0m' # No Color
+
+#echo -e "${YEL}${YEL}--${NC}Upgrading and installing useful PIP stuff"
+
+# Time on VM's can be incorrect after I resume. If you don't need this, comment it out
+echo -e "${YEL}----- UPDATING TIME -----${NC}"
 ntpdate time.nist.gov
 
-echo "-------------------------------"
-echo " Current Version Info Follows: "
-echo "-------------------------------"
+echo -e "${YEL}-------------------------------${NC}"
+echo -e "${YEL} Current Version Info Follows: "
+echo -e "${YEL}-------------------------------${NC}"
 lsb_release -i
 lsb_release -r
 lsb_release -d
 lsb_release -c
 printf "Kernal Version: ";uname -r
 printf "Processor Type: ";uname -m
-echo "------------------------------"
-echo "     Performing updates:      "
-echo "------------------------------"
-echo "----- CLEAN -----"
+echo -e "${YEL}------------------------------${NC}"
+echo -e "${YEL}     Performing updates:      ${NC}"
+echo -e "${YEL}------------------------------${NC}"
+echo -e "${YEL}----- CLEAN -----${NC}"
 apt clean
-echo "----- UPDATE -----"
+echo -e "${YEL}----- UPDATE -----${NC}"
 apt update
-echo "----- UPGRADE -----"
+echo -e "${YEL}----- UPGRADE -----${NC}"
 apt upgrade -y
-echo "----- DIST-UPGRADE -----"
+echo -e "${YEL}----- DIST-UPGRADE -----${NC}"
 apt full-upgrade -y
 # I've had this break text rendering. This was fixed with 'apt-get install --reinstall fonts-cantarell'
-echo "----- AUTOREMOVE -----"
+echo -e "${YEL}----- AUTOREMOVE -----${NC}"
 apt autoremove -y
-echo "------------------------------"
-echo " Device Version Info Follows: "
-echo "------------------------------"
+echo -e "${YEL}------------------------------${NC}"
+echo -e "${YEL} Device Version Info Follows: "
+echo -e "${YEL}------------------------------${NC}"
 lsb_release -i
 lsb_release -r
 lsb_release -d
 lsb_release -c
 printf "Kernal Version: ";uname -r
 printf "Processor Type: ";uname -m
-echo "------------------------------"
-echo "     Other Applications:      "
-echo "------------------------------"
-#echo "----- SNAP UPDATE -----"
+echo -e "${YEL}------------------------------${NC}"
+echo -e "${YEL}     Other Applications:      "
+echo -e "${YEL}------------------------------${NC}"
+#echo -e "${YEL}----- SNAP UPDATE -----${NC}"
 ## Uncomment this if you use snap
 #snap refresh
-echo "----- PIP -----"
+echo -e "${YEL}----- PIP -----${NC}"
 pip install --upgrade pip
-echo "----- WPSCAN -----"
+echo -e "${YEL}----- WPSCAN -----${NC}"
 wpscan --update
-echo "----- NIKTO -----"
+echo -e "${YEL}----- NIKTO -----${NC}"
 cd /usr/share/golismero/tools/nikto
 perl nikto.pl -update
 cd --
-#echo "----- FRESHCLAM -----"
+#echo -e "${YEL}----- FRESHCLAM -----${NC}"
 ## Uncomment this if you use ClamAV
 #freshclam
-#echo "----- NESSUS -----"
+#echo -e "${YEL}----- NESSUS -----${NC}"
 ## Uncomment if you use Nessus
 ## Don't be running scans... Stop the service, wait a few to make sure, upgrade, and start the service.
 #service nessusd stop
@@ -61,19 +72,16 @@ cd --
 #/opt/nessus/sbin/nessuscli update --all
 #sleep 3
 #service nessusd start
-echo "----- GITHUB -----"
+echo -e "${YEL}----- GITHUB -----${NC}"
 # The following -should- grab updates from any github projects added by new_setup.sh. If you add more or remove these, fix them.
 # Also, watch testssl since it doesn't use master and will likely change.
 cd /root/Scripts/Abeebus
 git pull origin master
 cd --
+cd /root/Scripts/Rebeebus
+git pull origin master
+cd --
 cd /root/Scripts/DidierStevensSuite
-git pull origin master
-cd --
-cd /root/Scripts/Empire
-git pull origin master
-cd --
-cd /root/Scripts/EyeWitness
 git pull origin master
 cd --
 cd /root/Scripts/kali-tools
@@ -94,25 +102,16 @@ cd --
 cd /root/Scripts/PRET
 git pull origin master
 cd --
-cd /root/Scripts/Sublist3r
-git pull origin master
-cd --
-cd /root/Scripts/testssl
-git pull origin 2.9dev
-cd --
 cd /root/Scripts/unix-privesc-check2
 git pull origin master
 cd --
 cd /root/Scripts/Windows-Exploit-Suggester
 git pull origin master
 cd --
-cd /root/Scripts/WPSeku
-git pull origin master
-cd --
-echo "----- REBOOT? -----"
+echo -e "${YEL}----- REBOOT? -----${NC}"
 if [ -f /var/run/reboot-required ]; then
-  echo 'Reboot Required'
+  echo -e "${RED}Reboot Required${NC}"
 else
-  echo 'Nothing Here'
+  echo -e "${GRN}Nothing Here${NC}"
 fi
-echo "----- FIN -----"
+echo -e "${YEL}----- FIN -----${NC}"
